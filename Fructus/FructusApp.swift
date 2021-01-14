c59f91519433e22ebe1258b16887f77acfd37747
kobayashi-kenta <12321klj@gmail.com>
2021-01-14 23:03:26 +0900

    firstcommit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kenta on 2021/01/14.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
